3e8bf990bc04d8737440980dba2e62b6da97f554
Zach <xuzhangde@gmail.com>
2022-03-18 14:09:31 -0700

    App Set up

---
+++ Stocks/Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Zhang Xu on 2022/3/18.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
